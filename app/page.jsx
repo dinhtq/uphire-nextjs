4bc89fba9d372707e9aa4967c0d32ad0576271ab
Jason Dinh <dinhtq1@gmail.com>
2025-05-16 21:17:41 -0700

    added test form

--- app/page.jsx
+++ app/page.jsx
@@ -41,6 +41,26 @@ export default function Page() {
                     <RuntimeContextCard />
                 </section>
             )}
+            <form name="contact" method="POST" data-netlify="true">
+                <p>
+                    <label>
+                        Your Name: <input type="text" name="name" />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Your Email: <input type="email" name="email" />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Message: <textarea name="message"></textarea>
+                    </label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
+            </form>
             <section className="flex flex-col gap-4">
                 <Markdown content={preDynamicContentExplainer} />
                 <RandomQuote />
