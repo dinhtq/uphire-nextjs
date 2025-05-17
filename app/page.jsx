5f9cec26f0fa3446126aed022fbab37491305d3b
Jason Dinh <dinhtq1@gmail.com>
2025-05-16 21:22:17 -0700

    using netlify attr

--- app/page.jsx
+++ app/page.jsx
@@ -41,7 +41,7 @@ export default function Page() {
                     <RuntimeContextCard />
                 </section>
             )}
-            <form name="contact" method="POST" data-netlify="true">
+            <form name="contact" method="POST" netlify>
                 <p>
                     <label>
                         Your Name: <input type="text" name="name" />
